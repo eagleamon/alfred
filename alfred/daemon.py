b2e86a650ec3eef90c173c6b8a8bfd396a3003da
joseph <joseph.piron@gmail.com>
2013-09-06 16:09:26 +0200

    with signal handler

--- alfred/daemon.py
+++ alfred/daemon.py
@@ -12,19 +12,6 @@ from alfred import bindings
 from pymongo import MongoClient
 
 
-def getAvailableBindings():
-    """
-    Check for all bindings available
-    TODO: make this more robust
-    """
-
-    res, bindingPath = [], bindings.__path__[0]
-    for d in os.listdir(bindingPath):
-        if os.path.isdir(os.path.join(bindingPath, d)):
-            res.append(d)
-    return res
-
-
 def parseArgs(sysArgs=''):
     """
     Parse arguments from command line and and returns a arg object, everything else comes from the db
@@ -41,17 +28,78 @@ def parseArgs(sysArgs=''):
 
     return parser.parse_args(sysArgs)
 
-def stop():
-    [x.stop() for x in activeBindings]
 
-def main():
+def getAvailableBindings():
     """
+    Check for all bindings available
+    TODO: make this more robust
     """
 
+    res, bindingPath = [], bindings.__path__[0]
+    for d in os.listdir(bindingPath):
+        if os.path.isdir(os.path.join(bindingPath, d)):
+            res.append(d)
+    return res
+
+
+class Alfred(object):
+
+    def __init__(self, db):
+        self.activeBindings = []
+        self.logger = logging.getLogger(__name__)
+        self.db = db
+
+    def signalHandler(self, signum, frame):
+        self.stop()
+
+    def stop(self):
+        [x.stop() for x in self.activeBindings]
+
+    def main(self):
+        """
+        """
+
+        # Get/create general config
+        config = self.db.config.find_one()
+        if not config:
+            self.db.config.insert({'brokerHost': 'test-gw', 'brokerPort': 1883})
+        config = self.db.config.find_one()
+
+        # Connect to the bus to get all updates and create central repository
+        bus = Bus(config.get('brokerHost'), config.get('brokerPort'))
+        bus.subscribe('#')
+
+        # Then register all available plugins and create/read their configuration
+        bindingDirs = getAvailableBindings()
+        logging.info("Available bindings: %s" % bindingDirs)
+
+        signal.signal(signal.SIGINT, self.signalHandler)
+
+        for bindingName in bindingDirs:
+            bindingDef = self.db.bindings.find_one({'name': bindingName})
+            if not bindingDef:
+                self.db.bindings.insert(dict(
+                    name=bindingName,
+                    installed=False,
+                    config={}
+                ))
+            else:
+                if bindingDef.get('installed'):
+                    logging.info("Starting binding %s" % bindingName)
+                    __import__('alfred.bindings.%s' % bindingName)
+                    b = bindings.Binding.plugins[bindingName](self.db)
+                    self.activeBindings.append(b)
+                    b.start()
+
+        # Then fetch item definition
+        items = self.db.items.find()
+        logging.info('Available items: %s' % list(items))
+
+        signal.pause()
+
+if __name__ == '__main__':
     # First parse the required options
     args = parseArgs(sys.argv[1:])
-    if args.debug:
-        logging.getLogger('').setLevel(logging.DEBUG)
 
     logging.basicConfig(
         level=logging.__getattribute__(args.debug and 'DEBUG' or 'INFO'),
@@ -60,45 +108,4 @@ def main():
     # Connection to the Database
     db = MongoClient(args.db_host, args.db_port)[args.db_name]
 
-    # Get/create general config
-    config = db.config.find_one()
-    if not config:
-        db.config.insert({'brokerHost': 'test-gw', 'brokerPort': 1883})
-    config = db.config.find_one()
-
-    # Connect to the bus to get all updates and create central repository
-    bus = Bus(config.get('brokerHost'), config.get('brokerPort'))
-    bus.subscribe('#')
-
-    # Then register all available plugins and create/read their configuration
-    bindingDirs = getAvailableBindings()
-    logging.info("Available bindings: %s" % bindingDirs)
-
-    activeBindings = []
-
-    signal.signal(signal.SIGINT, stop())
-
-    for bindingName in bindingDirs:
-        bindingDef = db.bindings.find_one({'name': bindingName})
-        if not bindingDef:
-            db.bindings.insert(dict(
-                name=bindingName,
-                installed=False,
-                config={}
-            ))
-        else:
-            if bindingDef.get('installed'):
-                logging.info("Starting binding %s" % bindingName)
-                __import__('alfred.bindings.%s' % bindingName)
-                b = bindings.Binding.plugins[bindingName](db)
-                activeBindings.append(b)
-                b.start()
-
-    # Then fetch item definition
-    items = db.items.find()
-    logging.info('Available items: %s' % list(items))
-
-    b.stop()
-
-if __name__ == '__main__':
-    main()
+    Alfred(db).main()
